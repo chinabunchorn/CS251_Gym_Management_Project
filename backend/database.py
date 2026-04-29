6fda962decec99613f1fb0c334449783b8396747
Akaradech <akaradech.mee@dome.tu.ac.th>
2026-04-29 13:04:09 +0700

    Adjust database.py

--- backend/database.py
+++ backend/database.py
@@ -1,13 +1,9 @@
 import mysql.connector
-import os
 
 def get_connection():
     return mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
-        port=int(os.getenv("DB_PORT", 4000)),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME"),
-        ssl_verify_cert=True,
-        ssl_verify_identity=True
+        host="localhost",
+        user="root",
+        password="",
+        database="gymmanagement"
     )
